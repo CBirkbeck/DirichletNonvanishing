5867b255f91556c1aafe9a07dc29f3f9444d0c35
David Loeffler <d.loeffler.01@cantab.net>
2024-10-14 21:33:46 +0200

    Nonnegativity of Dirichlet series coeffs

--- Project/MainTheorem.lean
+++ Project/MainTheorem.lean
@@ -8,38 +8,16 @@ variable {N : ℕ} [NeZero N] {χ : DirichletCharacter ℂ N}
 
 theorem mainTheorem_quadratic (hχ : χ ^ 2 = 1) (χ_ne : χ ≠ 1) : χ.LFunction 1 ≠ 0 := by
   intro hL
-  -- tedious step: show that `χ` comes from a real-valued char
-  obtain ⟨ξ, rfl⟩ : ∃ ξ : DirichletCharacter ℝ N, χ = ξ.ringHomComp ofRealHom := by
-    have (x : ZMod N) : (χ x).im = 0 := by
-      by_cases hx : IsUnit x
-      · have hx' : (χ x) ^ 2 = 1 := by
-          rw [← χ.pow_apply' two_ne_zero, hχ, MulChar.one_apply hx]
-        rw [sq_eq_one_iff] at hx'
-        rcases hx' with h | h <;>
-        · simp only [h, neg_im, one_im, neg_zero]
-      · rw [χ.map_nonunit hx, zero_im]
-    let ξ : DirichletCharacter ℝ N :=
-    { toFun := fun x ↦ (χ x).re,
-      map_one' := by simp only [map_one, one_re],
-      map_mul' := by
-        intro x y
-        simp only [map_mul, mul_re, sub_eq_self, mul_eq_zero, this, true_or]
-      map_nonunit' := by
-        intro u hu
-        simp only [χ.map_nonunit hu, zero_re] }
-    use ξ
-    ext a
-    simp only [MulChar.ringHomComp_apply, MulChar.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk,
-      ofRealHom_eq_coe, ξ]
-    conv_lhs => rw [← re_add_im (χ a), this, ofReal_zero, zero_mul, add_zero]
-  let B : BadChar N := {χ₀ := ξ, hχ := hL, χ_ne := χ_ne}
+  let B : BadChar N := {χ := χ, χ_sq := hχ, hχ := hL, χ_ne := χ_ne}
   exact B.elim
 
 variable (χ) in
-theorem ourMainTheorem {N : ℕ} [NeZero N] {χ : DirichletCharacter ℂ N} (χ_ne : χ ≠ 1) (t : ℝ) :
+theorem ourMainTheorem {N : ℕ} [NeZero N]
+    (χ : DirichletCharacter ℂ N) (t : ℝ) (hχt : χ ≠ 1 ∨ t ≠ 0) :
     χ.LFunction (1 + I * t) ≠ 0 := by
   by_cases h : χ ^ 2 = 1 ∧ t = 0
-  · rw [h.2, ofReal_zero, mul_zero, add_zero]
-    exact mainTheorem_quadratic h.1 χ_ne
+  · simp only [ne_eq, h.2, not_true_eq_false, or_false] at hχt
+    rw [h.2, ofReal_zero, mul_zero, add_zero]
+    exact mainTheorem_quadratic h.1 hχt
   · rw [not_and_or] at h
     exact mainTheorem_general h
