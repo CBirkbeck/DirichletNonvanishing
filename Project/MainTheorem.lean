922bb8f3fc33592af506c6563872579321879e44
David Loeffler <d.loeffler.01@cantab.net>
2024-10-14 16:31:35 +0200

    Proof of F differentiable

--- Project/MainTheorem.lean
+++ Project/MainTheorem.lean
@@ -1,4 +1,5 @@
 import Project.PropertiesF
+import Mathlib.NumberTheory.MulChar.Lemmas
 
 open Complex
 
@@ -7,8 +8,31 @@ variable {N : ℕ} [NeZero N] {χ : DirichletCharacter ℂ N}
 theorem mainTheorem_quadratic (hχ : χ ^ 2 = 1) :
     χ.LFunction 1 ≠ 0 := by
   intro hL
-  obtain ⟨ξ, rfl⟩ : ∃ ξ : DirichletCharacter ℝ N, χ = ξ.ringHomComp ofRealHom := by
-    sorry
+  -- tedious step: show that `χ` comes from a real-valued char
+  obtain ⟨ξ, rfl⟩ :
+      ∃ ξ : DirichletCharacter ℝ N, χ = ξ.ringHomComp ofRealHom := by
+    have (x : ZMod N) : (χ x).im = 0 := by
+      by_cases hx : IsUnit x
+      · have hx' : (χ x) ^ 2 = 1 := by
+          rw [← χ.pow_apply' two_ne_zero, hχ, MulChar.one_apply  hx]
+        rw [sq_eq_one_iff] at hx'
+        rcases hx' with h | h <;>
+        · simp only [h, neg_im, one_im, neg_zero]
+      · rw [χ.map_nonunit hx, zero_im]
+    let ξ : DirichletCharacter ℝ N :=
+    { toFun := fun x ↦ (χ x).re,
+      map_one' := by simp only [map_one, one_re],
+      map_mul' := by
+        intro x y
+        simp only [map_mul, mul_re, sub_eq_self, mul_eq_zero, this, true_or]
+      map_nonunit' := by
+        intro u hu
+        simp only [χ.map_nonunit hu, zero_re] }
+    use ξ
+    ext a
+    simp only [MulChar.ringHomComp_apply, MulChar.coe_mk, MonoidHom.coe_mk, OneHom.coe_mk,
+      ofRealHom_eq_coe, ξ]
+    conv_lhs => rw [← re_add_im (χ a), this, ofReal_zero, zero_mul, add_zero]
   let B : BadChar N := {χ₀ := ξ, hχ := hL}
   exact B.elim
 
