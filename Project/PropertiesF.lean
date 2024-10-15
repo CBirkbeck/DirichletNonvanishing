774c1ef518e775db69d1db1e6ab0548a00223d9a
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-10-15 17:29:41 +0200

    some cleanup, docstrings

--- Project/PropertiesF.lean
+++ Project/PropertiesF.lean
@@ -2,9 +2,13 @@ import Mathlib.NumberTheory.LSeries.DirichletContinuation
 import Mathlib.NumberTheory.LSeries.Dirichlet
 import Project.EulerProducts.Auxiliary
 
+/-!
+# Non-vanishing of `L(χ, 1)` for nontrivial quadratic characters `χ`
+-/
+
 open Complex
 
--- The object we're trying to show doesn't exist.
+/-- The object we're trying to show doesn't exist. -/
 structure BadChar (N : ℕ) [NeZero N] where
   χ : DirichletCharacter ℂ N
   χ_ne : χ ≠ 1
@@ -15,6 +19,7 @@ variable {N : ℕ} [NeZero N]
 
 noncomputable section
 
+/-- The associated character is quadratic. -/
 lemma BadChar.χ_apply_eq (B : BadChar N) (x : ZMod N) :
     B.χ x = 0 ∨ B.χ x = 1 ∨ B.χ x = -1 := by
   by_cases hx : IsUnit x
@@ -24,15 +29,14 @@ lemma BadChar.χ_apply_eq (B : BadChar N) (x : ZMod N) :
     tauto
   · simp only [B.χ.map_nonunit hx, true_or]
 
-/-- The auxiliary function `s ↦ ζ s * L B.χ s`. -/
-def BadChar.F (B : BadChar N) : ℂ → ℂ := Function.update
-  (fun s : ℂ ↦ riemannZeta s * B.χ.LFunction s) 1 (deriv B.χ.LFunction 1)
+/-- The auxiliary function `F: s ↦ ζ s * L B.χ s`. -/
+def BadChar.F (B : BadChar N) : ℂ → ℂ :=
+  Function.update (fun s : ℂ ↦ riemannZeta s * B.χ.LFunction s) 1 (deriv B.χ.LFunction 1)
 
 lemma BadChar.F_differentiableAt_of_ne (B : BadChar N) {s : ℂ} (hs : s ≠ 1) :
-    DifferentiableAt ℂ (B.F) s := by
+    DifferentiableAt ℂ B.F s := by
   apply DifferentiableAt.congr_of_eventuallyEq
-  · exact (differentiableAt_riemannZeta hs).mul <|
-      B.χ.differentiableAt_LFunction s (.inl hs)
+  · exact (differentiableAt_riemannZeta hs).mul <| B.χ.differentiableAt_LFunction s (.inl hs)
   · filter_upwards [eventually_ne_nhds hs] with t ht using Function.update_noteq ht ..
 
 lemma BadChar.F_differentiable (B : BadChar N) : Differentiable ℂ B.F := by
@@ -40,13 +44,12 @@ lemma BadChar.F_differentiable (B : BadChar N) : Differentiable ℂ B.F := by
   rcases ne_or_eq s 1 with hs | rfl
   · exact B.F_differentiableAt_of_ne hs
   · apply AnalyticAt.differentiableAt
-    apply Complex.analyticAt_of_differentiable_on_punctured_nhds_of_continuousAt
+    apply analyticAt_of_differentiable_on_punctured_nhds_of_continuousAt
     · filter_upwards [self_mem_nhdsWithin] with t ht
       exact B.F_differentiableAt_of_ne ht
     -- now reduced to showing *continuity* at s = 1
-    let G : ℂ → ℂ := Function.update (fun s ↦ (s - 1) * riemannZeta s) 1 1
-    let H : ℂ → ℂ := Function.update
-      (fun s ↦ B.χ.LFunction s / (s - 1)) 1 (deriv B.χ.LFunction 1)
+    let G := Function.update (fun s ↦ (s - 1) * riemannZeta s) 1 1
+    let H := Function.update (fun s ↦ B.χ.LFunction s / (s - 1)) 1 (deriv B.χ.LFunction 1)
     have : B.F = G * H := by
       ext t
       rcases eq_or_ne t 1 with rfl | ht
@@ -57,12 +60,10 @@ lemma BadChar.F_differentiable (B : BadChar N) : Differentiable ℂ B.F := by
     rw [this]
     apply ContinuousAt.mul
     · simpa only [G, continuousAt_update_same] using riemannZeta_residue_one
-    · have : HasDerivAt (B.χ.LFunction) (deriv B.χ.LFunction 1) 1 := by
-        apply DifferentiableAt.hasDerivAt
-        refine DirichletCharacter.differentiableAt_LFunction B.χ 1 (.inr B.χ_ne)
+    · have : HasDerivAt B.χ.LFunction (deriv B.χ.LFunction 1) 1 :=
+        (B.χ.differentiableAt_LFunction 1 (.inr B.χ_ne)).hasDerivAt
       rw [hasDerivAt_iff_tendsto_slope] at this
-      simp only [funext (slope_def_field B.χ.LFunction 1),
-        B.hχ, sub_zero] at this
+      simp only [funext (slope_def_field B.χ.LFunction 1), B.hχ, sub_zero] at this
       rw [Metric.continuousAt_iff']
       intro ε hε
       simp only [Metric.tendsto_nhds, eventually_nhdsWithin_iff] at this
@@ -71,18 +72,20 @@ lemma BadChar.F_differentiable (B : BadChar N) : Differentiable ℂ B.F := by
       · simp only [dist_self, hε]
       · simpa only [H, Function.update_noteq ha', Function.update_same] using ha ha'
 
+/-- The trivial zero at `s = -2` of the zeta function gives that `F (-2) = 0`.
+This is used later to obtain a contradction. -/
 lemma BadChar.F_neg_two (B : BadChar N) : B.F (-2) = 0 := by
   simp only [BadChar.F]
   have := riemannZeta_neg_two_mul_nat_add_one 0
   rw [Nat.cast_zero, zero_add, mul_one] at this
   rw [Function.update_noteq (mod_cast (by omega : (-2 : ℤ) ≠ 1)), this, zero_mul]
 
-/-- The real-valued arithmetic function whose L-series is `B.F`. -/
+/-- The complex-valued arithmetic function whose L-series is `B.F`. -/
 def BadChar.e (B : BadChar N) : ArithmeticFunction ℂ := .zeta * toArithmeticFunction (B.χ ·)
 
 lemma BadChar.F_eq_LSeries (B : BadChar N) {s : ℂ} (hs : 1 < s.re) :
     B.F s = LSeries (B.e ·) s := by
-  have (n : ℕ) : (B.e n : ℂ) = LSeries.convolution (fun _ ↦ 1) (B.χ ·) n := by
+  have (n : ℕ) : B.e n = LSeries.convolution (fun _ ↦ 1) (B.χ ·) n := by
     simp only [e, ArithmeticFunction.mul_apply, ArithmeticFunction.natCoe_apply,
       ArithmeticFunction.zeta_apply, Nat.cast_ite, Nat.cast_zero, Nat.cast_one, ite_mul, zero_mul,
       one_mul, LSeries.convolution_def]
@@ -92,8 +95,8 @@ lemma BadChar.F_eq_LSeries (B : BadChar N) {s : ℂ} (hs : 1 < s.re) :
   simp only [this]
   rw [LSeries_convolution', BadChar.F, Function.update_noteq]
   · congr 1
-    exact (LSeriesHasSum_one hs).LSeries_eq.symm
-    exact DirichletCharacter.LFunction_eq_LSeries _ hs
+    · exact (LSeriesHasSum_one hs).LSeries_eq.symm
+    · exact DirichletCharacter.LFunction_eq_LSeries _ hs
   · intro h
     simp only [h, one_re, lt_self_iff_false] at hs
   · rwa [← Pi.one_def, LSeriesSummable_one_iff]
@@ -109,7 +112,7 @@ lemma BadChar.mult_e (B : BadChar N) : B.e.IsMultiplicative := by
       if_false, Nat.cast_mul, map_mul]
 
 -- We use the ordering on `ℂ` given by comparing real parts for fixed imaginary part
-open ComplexOrder
+open scoped ComplexOrder
 
 lemma BadChar.e_prime_pow (B : BadChar N) {p : ℕ} (hp : p.Prime) (k : ℕ) :
     0 ≤ B.e (p ^ k) := by
@@ -134,23 +137,21 @@ lemma BadChar.e_nonneg (B : BadChar N) (n : ℕ) : 0 ≤ B.e n := by
 lemma BadChar.e_one_eq_one (B : BadChar N) : B.e 1 = 1 := by
   simp only [e, toArithmeticFunction, ArithmeticFunction.mul_apply, Nat.divisorsAntidiagonal_one,
     Prod.mk_one_one, ArithmeticFunction.natCoe_apply, ArithmeticFunction.zeta_apply, Nat.cast_ite,
-    CharP.cast_eq_zero, Nat.cast_one, ArithmeticFunction.coe_mk, mul_ite, mul_zero, ite_mul,
-    zero_mul, one_mul, Finset.sum_singleton, Prod.snd_one, one_ne_zero, ↓reduceIte, Prod.fst_one,
-    map_one]
+    Nat.cast_zero, Nat.cast_one, ArithmeticFunction.coe_mk, mul_ite, mul_zero, ite_mul, zero_mul,
+    one_mul, Finset.sum_singleton, Prod.snd_one, one_ne_zero, ↓reduceIte, Prod.fst_one, map_one]
 
 open ArithmeticFunction in
 open scoped LSeries.notation in
-lemma BadChar.e_summable {s : ℂ} (hs : 1 < s.re) (B : BadChar N) : LSeriesSummable (B.e ·) s := by
-  simp only [BadChar.e]
-  have h₁ := LSeriesSummable_zeta_iff.mpr hs
-  have h₂ := DirichletCharacter.LSeriesSummable_of_one_lt_re B.χ hs
-  have h₂' : LSeriesSummable (toArithmeticFunction (↗B.χ)) s := by
-    refine (LSeriesSummable_congr s fun {n} hn ↦ ?_).mp h₂
+lemma BadChar.e_summable (B : BadChar N) {s : ℂ} (hs : 1 < s.re) : LSeriesSummable (B.e ·) s := by
+  have : LSeriesSummable (toArithmeticFunction (↗B.χ)) s := by
+    refine (LSeriesSummable_congr s fun {n} hn ↦ ?_).mp <| B.χ.LSeriesSummable_of_one_lt_re hs
     simp only [toArithmeticFunction, coe_mk, hn, ↓reduceIte]
-  exact ArithmeticFunction.LSeriesSummable_mul h₁ h₂'
+  exact ArithmeticFunction.LSeriesSummable_mul (LSeriesSummable_zeta_iff.mpr hs) this
 
-open scoped ComplexOrder
-
+/-- If all values of a `ℂ`-valued arithmetic function are nonnegative reals and `x` is a
+real number in the domain of absolute convergence, then the `n`th iterated derivative
+of the associated L-series is nonnegative real when `n` is even and nonpositive real
+when `n` is odd. -/
 lemma derivs_from_coeffs (a : ArithmeticFunction ℂ) (hn : ∀ n, 0 ≤ a n) (x : ℝ)
     (h : LSeries.abscissaOfAbsConv (a ·) < x) (n : ℕ) :
     0 ≤ (-1) ^ n * iteratedDeriv n (LSeries (a ·)) x := by
@@ -179,29 +180,22 @@ lemma derivs_from_coeffs (a : ArithmeticFunction ℂ) (hn : ∀ n, 0 ≤ a n) (x
         · simp only [cpow_def, Nat.cast_eq_zero, hm, ↓reduceIte, ← natCast_log, ← ofReal_mul,
           exp_ofReal_im, neg_zero, zero_div]
 
-lemma BadChar.F_two_pos (B : BadChar N) : 0 < (B.F 2) := by
-  rw [B.F_eq_LSeries (s := 2) (hs:= by norm_num), LSeries]
-  apply tsum_pos (i := 1)
-  · rw [LSeries.term_def]
-    simp only [one_ne_zero, ↓reduceIte, e_one_eq_one, ofReal_one, Nat.cast_one, cpow_ofNat, one_pow,
-      ne_eq, not_false_eq_true, div_self, zero_lt_one]
-  · rw [← LSeriesSummable]
-    apply BadChar.e_summable (by norm_num)
-  · intro i
-    rw [LSeries.term_def]
-    simp only [cpow_ofNat]
-    rcases eq_or_ne i 0 with rfl | hi
-    · simp only [↓reduceIte, le_refl]
-    · simp only [hi, ↓reduceIte]
-      apply mul_nonneg
-      · exact B.e_nonneg i
-      · simp only [le_def, zero_re, inv_re, zero_im, inv_im]
-        constructor
-        · apply div_nonneg _ (normSq_nonneg _)
-          norm_cast
-          exact sq_nonneg i
-        · norm_cast
-          simp only [neg_zero, Nat.cast_pow, map_pow, normSq_natCast, zero_div]
+lemma BadChar.F_two_pos (B : BadChar N) : 0 < B.F 2 := by
+  rw [B.F_eq_LSeries (by norm_num), LSeries]
+  refine tsum_pos (B.e_summable (by norm_num)) (fun i ↦ ?_) 1 ?_ ; swap
+  · simp only [LSeries.term_def, one_ne_zero, ↓reduceIte, e_one_eq_one, Nat.cast_one, cpow_ofNat,
+      one_pow, ne_eq, not_false_eq_true, div_self, zero_lt_one]
+  · simp only [LSeries.term_def, cpow_ofNat]
+    split
+    · simp only [le_refl]
+    · refine mul_nonneg (B.e_nonneg i) ?_
+      simp only [le_def, zero_re, inv_re, zero_im, inv_im]
+      constructor
+      · apply div_nonneg _ (normSq_nonneg _)
+        norm_cast
+        exact sq_nonneg i
+      · norm_cast
+        simp only [neg_zero, Nat.cast_pow, map_pow, normSq_natCast, zero_div]
 
 lemma iteratedDeriv_comp_const_add (n : ℕ) (f : ℂ → ℂ) (s : ℂ) :
   iteratedDeriv n f s = iteratedDeriv n (fun z ↦ f (s + z)) 0 := by
