ea3562c134a0d6a9ad2551a6aae04e043d16a2c4
Michael Stoll <Michael.Stoll@uni-bayreuth.de>
2024-10-16 12:44:34 +0200

    more cleanup

--- Project/MainTheorem.lean
+++ Project/MainTheorem.lean
@@ -4,20 +4,21 @@ import Project.EasyCase
 
 open Complex
 
-variable {N : ℕ} [NeZero N] {χ : DirichletCharacter ℂ N}
+variable {N : ℕ} [NeZero N]
 
-theorem mainTheorem_quadratic (hχ : χ ^ 2 = 1) (χ_ne : χ ≠ 1) : χ.LFunction 1 ≠ 0 := by
+/-- If `χ` is a nontrivial quadratic Dirichlet character, then `L(χ, 1) ≠ 0`. -/
+theorem mainTheorem_quadratic {χ : DirichletCharacter ℂ N} (hχ : χ ^ 2 = 1) (χ_ne : χ ≠ 1) :
+    χ.LFunction 1 ≠ 0 := by
   intro hL
   let B : BadChar N := {χ := χ, χ_sq := hχ, hχ := hL, χ_ne := χ_ne}
   exact B.elim
 
-variable (χ) in
-theorem ourMainTheorem {N : ℕ} [NeZero N]
-    (χ : DirichletCharacter ℂ N) (t : ℝ) (hχt : χ ≠ 1 ∨ t ≠ 0) :
+/-- If `χ` is a Dirichlet character, then `L(χ, 1 + I*t)` does not vanish for `t ∈ ℝ`
+except when `χ` is trivial and `t = 0` (then `L(χ, s)` has a simple pole at `s = 1`). -/
+theorem ourMainTheorem (χ : DirichletCharacter ℂ N) (t : ℝ) (hχt : χ ≠ 1 ∨ t ≠ 0) :
     χ.LFunction (1 + I * t) ≠ 0 := by
   by_cases h : χ ^ 2 = 1 ∧ t = 0
   · simp only [ne_eq, h.2, not_true_eq_false, or_false] at hχt
     rw [h.2, ofReal_zero, mul_zero, add_zero]
     exact mainTheorem_quadratic h.1 hχt
-  · rw [not_and_or] at h
-    exact mainTheorem_general h
+  · exact mainTheorem_general <| not_and_or.mp h
--- Project/PropertiesF.lean
+++ Project/PropertiesF.lean
@@ -160,10 +160,10 @@ lemma BadChar.F_two_pos (B : BadChar N) : 0 < B.F 2 := by
 
 /-- The goal: bad characters do not exist. -/
 theorem BadChar.elim (B : BadChar N) : False := by
-  have h n (_ : n ≠ 0) :  0 ≤ (-1) ^ n * iteratedDeriv n B.F 2 := by
-    have hs : IsOpen {s : ℂ | 1 < s.re} := by refine isOpen_lt ?_ ?_ <;> fun_prop
-    convert B.e.iteratedDeriv_LSeries_alternating B.e_nonneg B.abscissa n using 2
-    convert iteratedDeriv_eq_on_open n hs ⟨(2 : ℂ), ?_⟩ fun _ ↦ B.F_eq_LSeries
-    simp only [Set.mem_setOf_eq, re_ofNat, Nat.one_lt_ofNat]
-  exact (B.F_two_pos.trans_le <|
-    B.F_neg_two ▸ apply_le_of_iteratedDeriv_alternating B.F_differentiable h (by norm_num)).false
+  refine (B.F_two_pos.trans_le <|
+    B.F_neg_two ▸
+      apply_le_of_iteratedDeriv_alternating B.F_differentiable (fun n _ ↦ ?_) (by norm_num)).false
+  have hs : IsOpen {s : ℂ | 1 < s.re} := by refine isOpen_lt ?_ ?_ <;> fun_prop
+  convert B.e.iteratedDeriv_LSeries_alternating B.e_nonneg B.abscissa n using 2
+  convert iteratedDeriv_eq_on_open n hs ⟨2, ?_⟩ fun _ ↦ B.F_eq_LSeries
+  simp only [Set.mem_setOf_eq, re_ofNat, Nat.one_lt_ofNat]
